e00bae7bd7d201133fd337e21b8b926f7c8e73c7
Thor <Thor@THUNDARPC>
2017-03-30 15:52:32 -0400

    Found how to get the .mdf file to Publish

--- Client_Desktop/Forms/HarvestForm.cs
+++ Client_Desktop/Forms/HarvestForm.cs
@@ -6,6 +6,7 @@ using Core.Utilities.Database.Queries.Tables;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.IO;
 using System.Linq;
 using System.Windows.Forms;
 
@@ -43,6 +44,21 @@ namespace Client_Desktop
             catch (Exception ex)
             {
                 MessageBox.Show(ex.Message);
+                if (ex.InnerException != null)
+                {
+                    using (StreamWriter writer = new StreamWriter(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "errorLog.txt")))
+                    {
+                        string lines = ex.Message;
+                        if (ex.InnerException != null)
+                        {
+                            lines += "\n" + ex.InnerException.Message;
+                            if (ex.InnerException.InnerException != null)
+                                lines += "\n" + ex.InnerException.InnerException.Message;
+                        }
+                        writer.WriteLine(lines);
+                    }
+                }
+                
             }
         }
 
