545f2876cc02ca7a938440f0173cdca21e01abd7
Thomas Knudson <knudsont6467@student.faytechcc.edu>
2017-04-23 17:49:19 -0400

    Change:  - Attempting to force LocalDB service to start before HarvestForm is created.

--- Client_Desktop/Program.cs
+++ Client_Desktop/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
@@ -17,7 +18,7 @@ namespace Client_Desktop
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-
+            ExecuteCommand("SqlLocalDB start HARVESTDATABASE.mdf");
             HarvestForm mainForm = null;
             try
             {
@@ -43,6 +44,30 @@ namespace Client_Desktop
                 }
                 mainForm.Dispose();
             }            
+        }
+
+        static void ExecuteCommand(string command)
+        {
+            var processInfo = new ProcessStartInfo("cmd.exe", "/c " + command);
+            processInfo.CreateNoWindow = true;
+            processInfo.UseShellExecute = false;
+            processInfo.RedirectStandardError = true;
+            processInfo.RedirectStandardOutput = true;
+
+            var process = Process.Start(processInfo);
+
+            process.OutputDataReceived += (object sender, DataReceivedEventArgs e) =>
+                Console.WriteLine("output>>" + e.Data);
+            process.BeginOutputReadLine();
+
+            process.ErrorDataReceived += (object sender, DataReceivedEventArgs e) =>
+                Console.WriteLine("error>>" + e.Data);
+            process.BeginErrorReadLine();
+
+            process.WaitForExit();
+
+            Console.WriteLine("ExitCode: {0}", process.ExitCode);
+            process.Close();
         }
     }
 }
