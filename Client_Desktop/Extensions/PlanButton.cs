4f0cc581137dfcc4a1581db6e36dc296e4f5181a
Thomas Knudson <knudsont6467@student.faytechcc.edu>
2017-04-23 12:24:58 -0400

    Change
    PlanButton.cs:
    - Added in a check to see if a planned meal exists on that day, for that
    meal time with the selected recipe. If it does: display a message box
    informing the user that they can't have the same recipe planned for the
    same meal time on the same day (Database currently does not support
    this); if not: add the planned meal as normal.

--- Client_Desktop/Extensions/PlanButton.cs
+++ Client_Desktop/Extensions/PlanButton.cs
@@ -31,10 +31,18 @@ namespace Client_Desktop.Extensions
                     meal.Date = dayPlanned;
                     meal.MealTime = mealTime;
 
-                    HarvestAdapter.PlannedMeals.Add(meal);
+                    if (HarvestAdapter.PlannedMeals.Contains(meal))
+                    {
+                        string errorMessage = string.Format("You already have {0} planned for {1} on {2}.", meal.PlannedRecipe.Name, meal.MealTime, meal.Date.DayOfWeek.ToString());
+                        MessageBox.Show(errorMessage, "Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    }
+                    else
+                    {
+                        HarvestAdapter.PlannedMeals.Add(meal);
 
-                    PlannedRecipeControl recipePrefab = new PlannedRecipeControl(meal);
-                    ((Button)sender).Parent.Controls.Add(recipePrefab);
+                        PlannedRecipeControl recipePrefab = new PlannedRecipeControl(meal);
+                        ((Button)sender).Parent.Controls.Add(recipePrefab);
+                    }
                 }
             }
         }
